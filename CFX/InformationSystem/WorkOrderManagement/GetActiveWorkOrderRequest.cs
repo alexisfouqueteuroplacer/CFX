b6ad068d67dec24c609ff910124a47c6a174ccab
alexisfouquet <alexis.fouquet@europlacer.fr>
2019-11-22 12:26:35 +0100

    Addition of the possibility for an EndPoint to "Activate" a Work Order

---
+++ CFX/InformationSystem/WorkOrderManagement/GetActiveWorkOrderRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CFX.Structures;
+
+namespace CFX.Production
+{
+    /// <summary>
+    /// Used to request work order that is activated at a process
+    /// endpoint. The response indicates the work order identifier
+    /// <code language="none">
+    /// {
+    ///   "Lane": 1,
+    ///   "Stage": null,
+    /// }
+    /// </code>
+    /// </summary>
+    public class GetActiveWorkOrderRequest : CFXMessage
+    {
+        /// <summary>
+        /// Number of the production lane (if applicable)
+        /// </summary>
+        public int? Lane
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An optional stage
+        /// </summary>
+        public Stage Stage
+        {
+            get;
+            set;
+        }
+    }
+}
