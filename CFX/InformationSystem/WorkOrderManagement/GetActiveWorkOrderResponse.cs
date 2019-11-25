d3c3a73f718aab56c87b45fa8824b273260e2c88
alexisfouquet <alexis.fouquet@europlacer.fr>
2019-11-25 16:46:41 +0100

    Addition of the possibility for an EndPoint to "Activate" a Work Order

---
+++ CFX/InformationSystem/WorkOrderManagement/GetActiveWorkOrderResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using CFX.Structures;
+
+namespace CFX.Production
+{
+    /// <summary>
+    /// Used to request work order that is activated at a process
+    /// endpoint. The response indicates the work order identifier
+    /// <code language="none">
+    /// {
+    ///   "Result": {
+    ///     "Result": "Success",
+    ///     "ResultCode": 0,
+    ///     "Message": "OK"
+    ///   },
+    ///   "WorkOrderIdentifier": {
+    ///     "WorkOrderId": "WO1122334455",
+    ///     "Batch": null
+    ///   }
+    /// }
+    /// </code>
+    /// </summary>
+    public class GetActiveWorkOrderResponse : CFXMessage
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GetActiveWorkOrderResponse()
+        {
+            Result = new RequestResult();
+        }
+
+        /// <summary>
+        /// The result of the request
+        /// </summary>
+        public RequestResult Result
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The identifer of the Work Order or Work Order sub-batch
+        /// </summary>
+        public WorkOrderIdentifier WorkOrderIdentifier
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CFX/InformationSystem/WorkOrderManagement/WorkOrderActivated.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CFX.Structures;
+
+namespace CFX.InformationSystem.WorkOrderManagement
+{
+    /// <summary>
+    /// Sent by a process endpoint to indicate the activation of a work order by its identifier
+    /// <code language="none">
+    /// {
+    ///   "WorkOrderIdentifier": {
+    ///     "WorkOrderId": "WO1122334455",
+    ///     "Batch": null
+    ///   },
+    ///   "Lane": 1,
+    ///   "Stage": null,
+    /// }
+    /// </code>
+    /// </summary>
+    public class WorkOrderActivated : CFXMessage
+    {
+        /// <summary>
+        /// The identifer of the Work Order or Work Order sub-batch
+        /// </summary>
+        public WorkOrderIdentifier WorkOrderIdentifier
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Number of the production lane (if applicable)
+        /// </summary>
+        public int? Lane
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// An optional stage
+        /// </summary>
+        public Stage Stage
+        {
+            get;
+            set;
+        }
+    }
+}
